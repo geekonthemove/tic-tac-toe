0c9442dec0f1016c4864a8e598d92877ff687d7a
Nour Jakobsen Halabi <code@kanvas.dk>
2019-01-05 12:20:59 +0100

    render board method, instead of hardcoded squares

--- src/index.js
+++ src/index.js
@@ -16,30 +16,34 @@ class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        key={i}
       />
     );
   }
 
+  renderBoard() {
+    let rows = [];
+
+    for (let i = 0; i < 3; i++) {
+      let children = [];
+
+      for (let j = 0; j < 3; j++) {
+        let square = this.renderSquare(i * 3 + j);
+        children.push(square);
+      }
+
+      rows.push(
+        <div className="board-row" key={i}>
+          {children}
+        </div>
+      );
+    }
+
+    return rows;
+  }
+
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.renderBoard()}</div>;
   }
 }
 
